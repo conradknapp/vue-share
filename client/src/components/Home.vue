a083d6f2a2e044fcc231faa97dbc0d69d9c89605
Conrad Knapp <knappco@bc.edu>
2018-08-12 09:57:58 -0400

    Installing Vuetify Plugin and Generating a Theme

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>Home</h1>
+    <v-btn color="error">Button</v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
-}
+  name: "home"
+};
 </script>
---
+++ theme.js
@@ -0,0 +1,9 @@
+theme: {
+  primary: "#3B125F",
+  secondary: "#8B5FBF",
+  accent: "#BF653F",
+  error: "#722530",
+  warning: "#A37513",
+  info: "#396893",
+  success: "#4caf50"
+}
