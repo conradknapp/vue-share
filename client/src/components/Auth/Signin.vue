3b97c725f3575843e25e43a9210ede13c57ac8fc
Conrad Knapp <knappco@bc.edu>
2018-08-12 21:14:02 -0400

    Using Variables in GraphQL, Signin / Signup Mutation Defs

--- client/src/components/Auth/Signin.vue
+++ client/src/components/Auth/Signin.vue
@@ -1,11 +1,62 @@
 <template>
-  <v-container>
-    <h1>Signin</h1>
+  <v-container mt-5 pt-5 text-xs-center>
+
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1>Welcome Back!</h1>
+      </v-flex>
+    </v-layout>
+
+    <!-- Signin Form -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card color="secondary" dark>
+          <v-card-text>
+            <v-container>
+              <v-form>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field v-model="username" prepend-icon="face" label="Username" type="text" required></v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field v-model="password" prepend-icon="extension" label="Password" type="password" required></v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit" color="accent">Signin</v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "Signin"
+  name: "Signin",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    handleSignin() {
+      this.$store.dispatch("signinUser", {
+        username: this.username,
+        password: this.password
+      });
+    }
+  }
 };
 </script>
