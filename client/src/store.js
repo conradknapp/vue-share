8d426ded1b689ef8a0b091757ada80047469f3ff
Conrad Knapp <knappco@bc.edu>
2018-08-13 13:08:27 -0400

    Protected Routes and Clearing Malformed Tokens

--- client/src/store.js
+++ client/src/store.js
@@ -60,6 +60,8 @@ export default new Vuex.Store({
         });
     },
     signinUser: ({ commit }, payload) => {
+      // clear token to prevent errors (if malformed)
+      localStorage.setItem("token", "");
       apolloClient
         .mutate({
           mutation: SIGNIN_USER,
