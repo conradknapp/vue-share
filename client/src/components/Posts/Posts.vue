c89df36c27751a9c401723b622be9437803ae567
Conrad Knapp <knappco@bc.edu>
2018-08-14 18:22:24 -0400

    Create Post Component and Route

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -4,7 +4,7 @@
     <!-- Post Cards -->
     <v-layout row wrap v-if="infiniteScrollPosts">
       <v-flex xs12 sm6 v-for="post in infiniteScrollPosts.posts" :key="post._id">
-        <v-card hover>
+        <v-card @click.native="goToPost(post._id)" hover>
           <v-card-media :src="post.imageUrl" height="30vh" lazy></v-card-media>
 
           <v-card-actions>
@@ -82,6 +82,9 @@ export default {
     }
   },
   methods: {
+    goToPost(postId) {
+      this.$router.push(`/posts/${postId}`);
+    },
     showMorePosts() {
       this.pageNum += 1;
       // fetch more data and transform original result
