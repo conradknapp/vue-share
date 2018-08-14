dbe1a82a184584d64f69f86f4d59b12828885449
Conrad Knapp <knappco@bc.edu>
2018-08-14 00:39:36 -0400

    Create and Execute addPost Action / Mutation

--- client/src/components/Posts/AddPost.vue
+++ client/src/components/Posts/AddPost.vue
@@ -94,6 +94,24 @@ export default {
           desc.length < 200 || "Description must have less than 200 characters"
       ]
     };
+  },
+  computed: {
+    ...mapGetters(["loading", "user"])
+  },
+  methods: {
+    handleAddPost() {
+      if (this.$refs.form.validate()) {
+        // add post action
+        this.$store.dispatch("addPost", {
+          title: this.title,
+          imageUrl: this.imageUrl,
+          categories: this.categories,
+          description: this.description,
+          creatorId: this.user._id
+        });
+        this.$router.push("/");
+      }
+    }
   }
 };
 </script>
