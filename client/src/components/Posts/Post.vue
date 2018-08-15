a74df1cc3f6681dbe439ebfb73a95b56d8a2b810
Conrad Knapp <knappco@bc.edu>
2018-08-14 21:51:23 -0400

    Add Messages Section to Post Component

--- client/src/components/Posts/Post.vue
+++ client/src/components/Posts/Post.vue
@@ -37,6 +37,57 @@
       </v-flex>
     </v-layout>
 
+    <!-- Messages Section -->
+    <div class="mt-3">
+      <!-- Message Input -->
+      <v-layout class="mb-3" v-if="user">
+        <v-flex xs12>
+          <v-form>
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field clearable append-outer-icon="send" label="Add Message" type="text" prepend-icon="email" required></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-form>
+        </v-flex>
+      </v-layout>
+
+      <!-- Messages -->
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-list subheader two-line>
+            <v-subheader>Messages ({{getPost.messages.length}})</v-subheader>
+
+            <template v-for="message in getPost.messages">
+              <v-divider :key="message._id"></v-divider>
+
+              <v-list-tile avatar inset :key="message.title">
+                <v-list-tile-avatar>
+                  <img :src="message.messageUser.avatar">
+                </v-list-tile-avatar>
+
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{message.messageBody}}
+                  </v-list-tile-title>
+                  <v-list-tile-sub-title>
+                    {{message.messageUser.username}}
+                    <span class="grey--text text--lighten-1 hidden-xs-only">{{message.messageDate}}</span>
+                  </v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-action class='hidden-xs-only'>
+                  <v-icon color="grey">chat_bubble</v-icon>
+                </v-list-tile-action>
+
+              </v-list-tile>
+            </template>
+          </v-list>
+        </v-flex>
+      </v-layout>
+
+    </div>
+
   </v-container>
 </template>
 
